4f90dc7475ba6a9987a0157822f3fe0e99ede55d
parthpatel133 <prpatel133@gmail.com>
2020-12-23 00:02:14 +0530

    simple form creation

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,7 +1,24 @@
+import {useState} from 'react';
+
 const CoursesPage = () => {
+  const [courseTitle, setCourseTitle] = useState('');
+
+  const handleChange = (e) => {
+    setCourseTitle(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setCourseTitle('');
+  };
   return (
     <div>
-      <h2>Courses</h2>
+      <form onSubmit={handleSubmit}>
+        <h2>Courses</h2>
+        <h3>Add Courses</h3>
+        <input type='text' onChange={handleChange} value={courseTitle} />
+        <input type='submit' value='Save' />
+      </form>
     </div>
   );
 };
