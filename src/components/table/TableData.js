487a547ee320f087d02ed766d2703941919b29e7
parthpatel133 <prpatel133@gmail.com>
2020-12-25 22:46:04 +0530

    fix bug

--- src/components/table/TableData.js
+++ src/components/table/TableData.js
@@ -3,7 +3,6 @@ import {useSelector} from 'react-redux';
 const TableData = () => {
   const courses = useSelector((state) => state.courses);
 
-  console.log(courses);
   return JSON.stringify(courses);
 };
 export default TableData;
