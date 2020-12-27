d4bc7ae5061dc583b2c3fdda1d2e445595629c3c
parthpatel133 <prpatel133@gmail.com>
2020-12-27 09:59:25 +0530

    initial setup for CRUD

--- src/components/PageNotFound.js
+++
@@ -1,3 +0,0 @@
-const PageNotFound = () => <h1>Oops! Page not found.</h1>;
-
-export default PageNotFound;
--- src/components/table/TableData.js
+++
@@ -1,8 +0,0 @@
-import {useSelector} from 'react-redux';
-
-const TableData = () => {
-  const courses = useSelector((state) => state.courses);
-
-  return JSON.stringify(courses);
-};
-export default TableData;
