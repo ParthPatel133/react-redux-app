549116cf4e6bf071d135dd394916b0aa91bccb35
parthpatel133 <prpatel133@gmail.com>
2021-01-01 17:54:09 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
-import {Route, Switch} from 'react-router-dom';
-import HomePage from './components/home/HomePage';
-import AboutPage from './components/about/AboutPage';
-import Header from './components/common/Header';
-import PageNotFound from './components/PageNotFound';
-import CoursesPage from './components/courses/CoursesPage';
-import DataPage from './components/courses/DataPage';
-import TableData from './components/table/TableData';
-import Table from './components/table/Table';
-function App() {
+// import './styles.css';
+import Products from './Products';
+
+export default function App() {
   return (
-    <div className='container-fluid'>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/about' component={AboutPage} />
-        <Route path='/courses' component={CoursesPage} />
-        <Route path='/data' component={DataPage} />
-        <Route path='/tabledata' component={TableData} />
-        <Route path='/finaltable' component={Table} />
-        <Route component={PageNotFound} />
-      </Switch>
+    <div className='App'>
+      <h1>hello world</h1>
     </div>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -2,19 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
-import configureStore from './redux/store';
-import {Provider} from 'react-redux';
-
-const store = configureStore();
-//passing initial state to the configureStore overWrite the redux store. ex: localStorage
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
